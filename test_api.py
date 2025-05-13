9ce9fd06a88425f67ee850ebf3f06429f1d8c939
Camila <110406487+Sharneya@users.noreply.github.com>
2025-05-13 10:18:05 +0500

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,3 +1,7 @@
+import requests
+from jsonschema import validate
+import pytest
+
 def test_get_users():  
     response = requests.get("https://reqres.in/api/users?page=2")  
     assert response.status_code == 200  
